d461c947c1047c933a36caf5ae8e8b90bbc1148a
Joao Roberto <joao.r.lopesdasilva@gmail.com>
2020-04-24 01:56:08 -0300

    :arrow_up: Dependencies and yarn.lock stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
+
+import Routes from './routes';
 
 import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
     <AppProvider>
-      <SignIn />
+      <Router>
+        <Routes />
+      </Router>
     </AppProvider>
     <GlobalStyle />
   </>
