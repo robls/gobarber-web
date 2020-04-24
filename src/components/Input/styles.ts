ea7210ab583317392096007cf1454fefd7ef1289
Joao Roberto <joao.r.lopesdasilva@gmail.com>
2020-04-23 23:14:49 -0300

    :sparkles: Toast and Tooltip components implemented

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,15 +1,46 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
+
   border: 2px solid #232129;
   color: #666360;
+
   padding: 16px;
   width: 100%;
 
   display: flex;
   align-items: center;
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+      color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
+
 
   input {
     flex: 1;
@@ -20,7 +51,6 @@ export const Container = styled.div`
     &::placeholder {
       color: #666360;
     }
-
   }
 
   & + div {
@@ -29,5 +59,22 @@ export const Container = styled.div`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background-color: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
